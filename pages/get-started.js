a7f026d569b97f9f3e13505663590446301ec3b3
BaekSangwoo <qortkdn@naver.com>
2019-09-01 14:59:56 +0900

    first deploy

---
+++ pages/get-started.js
@@ -0,0 +1,19 @@
+import Nav from '../components/nav';
+
+export default ()=>{
+
+    return(
+       
+    <>
+        <Nav/>
+        <h1>시작하기</h1>
+        <p>
+            Spring을 이용해서 #Rest API를 실행합니다.
+        </p>
+
+        <code>
+            npm -i g code-lab
+        </code>
+    </>
+    );
+}
